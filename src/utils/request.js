92517d93a13f3f02214db78dc3833d916478b5dd
zhengwujia <418894252@qq.com>
2021-01-03 22:36:48 +0800

    '对接后端登录几口，并做优化调试'

--- src/utils/request.js
+++ src/utils/request.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 // 创建一个axios实例，说白了就是复制一个oxious
 // 我们通过一个实例发请求，把需要的配置配个这个实例
 const request = axios.create({
-  baseURL: 'http://ttapi.research.itcast.cn/mp/' // 请求的基础路径
+  baseURL: 'http://api-toutiao-web.itheima.net/' // 请求的基础路径
 })
 
 // 请求拦截器
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,16 +1,57 @@
 <template>
   <div class="login-container">
-    <h1>登录侧二十</h1>
+    <!--
+    el-from 表单组件，每个表单项都必须使用 el-from-item 组件包裹
+    -->
+    <div class="login-form-wrap">
+      <div class="login-head">
+        <div class="logo"></div>
+      </div>
+    <el-form class="login-form" ref="form" :model="user" @keyup.enter.native="onLogin" >
+      <el-form-item >
+        <el-input
+          v-model="user.mobile"
+          placeholder="请输入手机号"
+        ></el-input>
+      </el-form-item>
+      <el-form-item >
+        <el-input
+          v-model="user.code"
+          placeholder="请输入验证码"
+        ></el-input>
+      </el-form-item>
+      <el-form-item >
+        <el-checkbox v-model="check">我已阅读并同意用户协议和隐私条款</el-checkbox>
+      </el-form-item>
+      <el-form-item>
+        <el-button
+          class="login-btn"
+          type="primary"
+          :loading="loginLoading"
+          @click="onLogin"
+        >登录</el-button>
+      </el-form-item>
+    </el-form>
+    </div>
   </div>
 </template>
 
 <script>
+  import request from '@/utils/request'
   export default {
     name: 'Login',
     components: {},
     props: {},
     data () {
-      return {}
+      return {
+        user: {
+          mobile: '',
+          code: ''
+        },
+        check: false, // 是否同意协议的选中状态
+        loginLoading: false // 开启加载状态
+
+      }
     },
     computed: {},
     watch: {},
@@ -19,10 +60,62 @@
     mounted () {
 
     },
-    methods: {}
+    methods: {
+      onLogin () {
+        // 获取表单数据（根据接口要求绑定数据）
+        const user = this.user
+        console.log(user, 'user')
+        this.loginLoading = true
+        request({
+          method: 'POST',
+          url: '/mp/v1_0/authorizations',
+          data: user
+        }).then(res => {
+           console.log(res, 'res')
+          this.loginLoading = false
+        this.$message.success('登录成功:)')
+        }).catch(err => {
+          this.loginLoading = false
+          console.log('错误信息是!', err)
+          this.$message.error('登录失败:(')
+        })
+      }
+    }
   }
 </script>
 
 <style scoped lang="less">
-
+  .login-container {
+    position: fixed;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: url("./login_bg.jpg") no-repeat;
+    background-size: cover;
+    .login-form-wrap {
+      min-width: 300px;
+      padding: 30px 50px 10px;
+      background-color: #fff;
+      .login-head {
+        display: flex;
+        justify-content: center;
+        .logo {
+          width: 200px;
+          height: 57px;
+          background: url("./logo_index.png") no-repeat;
+          background-size: contain;
+        }
+      }
+      .login-form {
+        .login-btn {
+          width: 100%;
+        }
+      }
+    }
+  }
 </style>
