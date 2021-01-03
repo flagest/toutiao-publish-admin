564194e1b96ffdca42ee95c78deca6af31e46efa
zhengwujia <418894252@qq.com>
2021-01-03 10:03:20 +0800

    测试git是否连接

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false // 关闭生产环境提示
 
+// 创建view根实例
 new Vue({
   router,
   render: h => h(App)
